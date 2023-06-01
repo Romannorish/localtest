7352331d7441e0f22e1f1a00db18a5c412ee5725
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2023-06-01 15:45:14 +0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import {Container} from "./App.styled"
+
+
+function App() {
+  return (
+    <Container>
+      <img src="https://omega.page/img/pageimg/banner1.jpg" alt="" />
+    </Container>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Header} from "./header/Header.styled"
+
+export default class Header extends React.Component{
+
+    render(){
+        return <Header>
+            
+        </Header>
+    }
+}
